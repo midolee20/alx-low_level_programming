8ee6c8790a0188f62d52720e9644ad310823cc1b
oyetola hammed <midoleecouture@gmail.com>
2023-09-07 18:38:47 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This program makes use of puts
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
