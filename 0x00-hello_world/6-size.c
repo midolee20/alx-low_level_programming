a30b1c9f0ad1050643424b798406ed9e73768cc6
oyetola hammed <midoleecouture@gmail.com>
2023-09-07 19:22:00 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,20 +4,19 @@
  * Description - This program prints various sizes the computer is compiled on
  * REturn: 0 (successful)
  */
+
 int main(void)
 {
 	char a;
 	int b;
 	long int c;
 	long long int d;
-	float f
-		;
+	float f;
+
 	printf("Size of a char: %u byte(s)\n", sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", sizeof(b));
- 	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
-
-
