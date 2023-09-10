ac2aa9fc4dfd8397d763525fa18b429b2cf41c49
oyetola hammed <midoleecouture@gmail.com>
2023-09-10 11:50:26 -0700

    combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,22 +8,22 @@
 
 int main(void)
 {
-	int i;
-	int j;
-	int k;
+	int i = '0';
+	int j = '0';
+	int k = '0';
 
-	for (i = '0'; i <= '9'; i++)
+	for (k = '0'; k <= '9'; k++)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-			for (k = '0'; k <= '9'; k++)
+			for (i = '0'; i <= '9'; i++)
 			{
 				if (!((i == j) || (j == k) || (j > i) || (k > j)))
 				{
 					putchar(k);
 					putchar(j);
 					putchar(i);
-					if (!(i == '9' && j == '8' && k == '7'))
+					if (!(i == '9' && k == '7' && j == '8'))
 					{
 						putchar(',');
 						putchar(' ');
