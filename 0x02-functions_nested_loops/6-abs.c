ff20502466a97a5d6a6a31f16b0d912c3b8c1a43
oyetola hammed <midoleecouture@gmail.com>
2023-09-12 18:40:38 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Program that prints absolute value
+ * @n: this is a parameter
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
