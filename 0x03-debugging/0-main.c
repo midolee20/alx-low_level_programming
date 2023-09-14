25daaab8670993df9935d277dc3604a4ccc09366
oyetola hammed <midoleecouture@gmail.com>
2023-09-14 16:16:31 -0700

    deb

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - tests function that prints if integer
+ * is positive or negative
+ * @i: this is a variable
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
