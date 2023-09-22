9e8f135034719f44abda8f4e99e321dcd601d084
oyetola hammed <midoleecouture@gmail.com>
2023-09-21 18:11:08 -0700

    lll

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * _strcmp - compares two strings
+ * @s1: warrior 1
+ * @s2: warrior 2
+ *
+ * Return: the needed
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
