cbf3bbb636cb8655eb629a4d3aab551c8a4a06eb
oyetola hammed <midoleecouture@gmail.com>
2023-09-21 18:24:25 -0700

    jjj

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - turn lowercase to uppercase
+ * @s: pointer
+ *
+ *  @s: pointer
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
