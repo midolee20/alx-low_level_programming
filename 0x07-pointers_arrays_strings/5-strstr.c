d375362b5b3f3cb5eac98c68fc1158330434cf0c
oyetola hammed <midoleecouture@gmail.com>
2023-09-25 18:15:11 -0700

    kpk

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point of program
+ * @haystack: input
+ * @needle: input
+ * Return: o
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
