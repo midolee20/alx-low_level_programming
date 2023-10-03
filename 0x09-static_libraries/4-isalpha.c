5b4623b8e9ea3991a1cfd2b95df36577665137b5
oyetola hammed <midoleecouture@gmail.com>
2023-10-02 18:55:43 -0700

    kkkl

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - dqzdqzdqzdqzdllllllllllll
+ * @c: Description of parameter x
+ *
+ * Description: ddzqdqzdlllllllllllll
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <=  90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - dqzdqzdqzdqzdjjjjjjjjjjj
+ * @n: Description of parameter x
+ *
+ * Description: ddzqdqzdlllll
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -(n);
+	}
+	return (n);
+}
