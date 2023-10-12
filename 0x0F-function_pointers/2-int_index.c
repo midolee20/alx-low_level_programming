5ba1fc57153432b21226c3a753ff794e22ee768f
oyetola hammed <midoleecouture@gmail.com>
2023-10-11 19:21:36 -0700

    jj

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,7 +5,7 @@
 /**
  * int_index - return index place if comparison = true, else -1
  * @array: array
- * @size: size of elements in array 
+ * @size: size of elements in array
  * @cmp: pointer to func of one of the 3 fuctions in main
  * Return: 0
  */
