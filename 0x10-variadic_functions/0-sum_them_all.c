433cfcbf0a8986d4e6c70e8e87fa35ed1f7506bd
oyetola hammed <midoleecouture@gmail.com>
2023-10-12 19:01:49 -0700

    jj

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - check the code for Holberton School 
+ * sum_them_all - check the code for Holberton School
  * @n: number of arguments
  * Return: Always 0.
  */
